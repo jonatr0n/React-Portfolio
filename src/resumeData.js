93388af19f3f59576a18f9fdadb5ed55304da908
Jonathan T <jhtran.dev@gmail.com>
2019-06-02 23:08:35 -0500

    added code into repo

---
+++ src/resumeData.js
@@ -0,0 +1,123 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Jonathan Tran",
+    "role": "Frontend Developer",
+    "linkedinId":"https://www.linkedin.com/in/jonathan-tran-phr-mba-54703861/",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I enjoy frontend development and styling and I love to develop little games / projects with new technologies. Sometimes I simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/jonathan-tran-phr-mba-54703861/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/jonatr0n?tab=repositories",
+          "className":"fa fa-github"
+        },
+        // {
+        //   "name":"skype",
+        //   "url":"http://twitter.com/rbhatia46",
+        //   "className":"fa fa-twitter"
+        // }
+      ],
+    "aboutme":"I am currently an independent contractor and have very recently completed the Full Stack Development course at SMU in Dallas, TX. Though new to the field, I have found enjoyment in learning and applying everything I have learned and plan to continue to consume new languages / libraries and output value added applications. I am currently seeking a full time position that will help me continue to grow in the field.",
+    "address":"Dallas, Tx, United States",
+    "email": "jhtran.dev@gmail.com",
+    "website":"",
+    "education":[
+      {
+        "UniversityName":"Southern Methodist University",
+        "specialization":"Full Stack Development",
+        "MonthOfPassing":"June",
+        "YearOfPassing":"2019",
+        "Achievements":""
+      },
+      {
+        "UniversityName":"University of Houston",
+        "specialization":"Master's in Human Resources Management",
+        "MonthOfPassing":"August",
+        "YearOfPassing":"2014",
+        "Achievements":"PHR completed December 2014"
+      },
+      {
+        "UniversityName":"University of Houston",
+        "specialization":"Bachelor's in Bustiness Marketing",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2009",
+        "Achievements":""
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Independent Contractor",
+        "specialization":"Completed Harvest API projects for Thought Ensemble Created HTML, CSS, JavaScript Form Applications for recruiting companies Met with various potential clients in the telecommunications / low voltage field to improve / develop their webpages ",
+        "MonthOfLeaving":"Present",
+        "YearOfLeaving":"",
+        "Achievements":""
+      },
+      {
+        "CompanyName":"Anistar Technologies",
+        "specialization":"Experience in speaking technically with full stack development contractors Familiarity with HTML, CSS, JavaScript, MySQL job requirements Identify key needs of Telecommunication Companies Identify Talent in key categories and areas Most importantly, identify key talents and differentiators in candidates Build, maintain, and cultivate relationships with talent and telecommunication clients Organize relationship pipeline and various lines of communications to fill client needs as soon as possible ",
+        "MonthOfLeaving":"April",
+        "YearOfLeaving":"2019",
+        "Achievements":""
+      }
+    ],
+    "skillsDescription":"Visual of knowledge base",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"JavaScript"
+      },
+      {
+        "skillname":"MySQL"
+      },
+      {
+        "skillname":"Material-UI"
+      },
+    ],
+    "portfolio":[
+      {
+        "name":"Pawder",
+        "description":"Application to find adoptable best friends based on zipcode",
+        "imgurl":"images/portfolio/coffee.jpg"
+      },
+      {
+        "name":"Happy Hour Finder",
+        "description":"A social application where users can easily and quickly share their favorite happy hours",
+        "imgurl":"images/portfolio/coffee.jpg"
+      },
+      {
+        "name":"Worksheet Generator",
+        "description":"Final Project for SMU Full Stack Devlopment Course. Allows teachers to quickly create math worksheets, quizzes and tests.",  
+        "imgurl":"images/portfolio/coffee.jpg"
+      },
+      {
+        "name":"React Monster Hunter Click Game",
+        "description":"Clicking game based on the popular game Monster Hunter",
+        "imgurl":"images/portfolio/coffee.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
